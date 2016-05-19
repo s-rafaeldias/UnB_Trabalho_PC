ed21ec47c8bd72b16eeee6f2e2fd2b7ecd67940f
Rafael Dias <rafa197795@gmail.com>
2016-05-19 14:41:12 -0300

    .

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 #include <semaphore.h>
 
 #define CHAPAS 100
-#define MAQ1 2
+#define MAQ1 3
 #define MAQ2 5
 #define Y 10
 #define X 200
@@ -19,7 +19,8 @@
 int chapasAluminio = CHAPAS;
 int latasBasicas = 0;
 
-sem_t mutex;
+
+sem_t mutexChapas;
 
 pthread_mutex_t lockChapas       = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t lockLatasBasicas = PTHREAD_MUTEX_INITIALIZER;
@@ -30,18 +31,14 @@ pthread_cond_t condChapaAluminio = PTHREAD_COND_INITIALIZER;
 void* maqChapaAlumunio(void* id) {
     int i = *((int*)id);
     while (TRUE) {
-        //pthread_mutex_lock(&lockChapas);
-        //printf("maqChapaAluminio %d pegou o lock\n", i);
-        //sleep(1);
-        sem_wait(&mutex);
+        sem_wait(&mutexChapas);
             if (chapasAluminio < CHAPAS) {
                 chapasAluminio += 10;
                 printf("MaqChapaAluminio %d, Quantidade de chapas de aluminio: %d\n", i,
                        chapasAluminio);
                 sleep(1);
             }
-        //pthread_mutex_unlock(&lockChapas);
-        sem_post(&mutex);
+        sem_post(&mutexChapas);
         sleep(1);
     }
 }
@@ -49,18 +46,14 @@ void* maqChapaAlumunio(void* id) {
 void* maqFazerLatinha(void* id) {
     int i = *((int*)id);
     while (TRUE) {
-        //pthread_mutex_lock(&lockChapas);
-        //printf("maqFazerLatinha %d pegou o lock\n", i);
-        //sleep(1);
-        sem_wait(&mutex);
+        sem_wait(&mutexChapas);
             if (chapasAluminio >= Y) {
                 chapasAluminio -= Y;
                 latasBasicas += X;
                 printf("MaqFazerLatinha %d, Quantidade de latas: %d\n", i, latasBasicas);
             }
-        sem_post(&mutex);
+        sem_post(&mutexChapas);
         sleep(1);
-        //pthread_mutex_unlock(&lockChapas);
     }
 }
 
@@ -71,19 +64,21 @@ int main() {
     pthread_t maq1[MAQ1];
     pthread_t maq2[MAQ2];
 
-    sem_init(&mutex, 0, 1);
+    sem_init(&mutexChapas, 0, 1);
 
-    for (int i = 0; i < MAQ2; i++) {
-        id = (int*) malloc(sizeof(int));
-        *id = i;
-        pthread_create(&maq2[i], NULL, maqFazerLatinha, (void*)id);
-    }
+    // Loops para a criação da threads
     for (int i = 0; i < MAQ1; i++) {
         id = (int*) malloc(sizeof(int));
         *id = i;
         pthread_create(&maq1[i], NULL, maqChapaAlumunio, (void*)id);
     }
+    for (int i = 0; i < MAQ2; i++) {
+        id = (int*) malloc(sizeof(int));
+        *id = i;
+        pthread_create(&maq2[i], NULL, maqFazerLatinha, (void*)id);
+    }
 
+    // Loop de join das threads
     for (int i = 0; i < MAQ1; i++) {
         pthread_join(maq1[i], NULL);
     }
