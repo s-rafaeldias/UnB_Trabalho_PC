090adca6bae9fb62856a2e6d01d95b875f3fc449
Rafael Dias <rafa197795@gmail.com>
2016-05-19 11:36:26 -0300

    Ajuste do starvation, trocando variavel de condição por semáforos

--- main.c
+++ main.c
@@ -6,10 +6,12 @@
 #include <pthread.h>
 #include <stdio.h>
 #include <zconf.h>
+#include <stdlib.h>
+#include <semaphore.h>
 
 #define CHAPAS 100
-#define MAQ1 5
-#define MAQ2 1
+#define MAQ1 2
+#define MAQ2 5
 #define Y 10
 #define X 200
 #define TRUE 1
@@ -17,58 +19,69 @@
 int chapasAluminio = CHAPAS;
 int latasBasicas = 0;
 
-pthread_mutex_t lockChapas = PTHREAD_MUTEX_INITIALIZER;
+sem_t mutex;
+
+pthread_mutex_t lockChapas       = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t lockLatasBasicas = PTHREAD_MUTEX_INITIALIZER;
 
 pthread_cond_t condFazerLatinhas = PTHREAD_COND_INITIALIZER;
 pthread_cond_t condChapaAluminio = PTHREAD_COND_INITIALIZER;
 
-void* maqChapaAlumunio() {
+void* maqChapaAlumunio(void* id) {
+    int i = *((int*)id);
     while (TRUE) {
-        pthread_mutex_lock(&lockChapas);
-            if (chapasAluminio <= Y) {
+        //pthread_mutex_lock(&lockChapas);
+        //printf("maqChapaAluminio %d pegou o lock\n", i);
+        //sleep(1);
+        sem_wait(&mutex);
+            if (chapasAluminio < CHAPAS) {
                 chapasAluminio += 10;
-                printf("Quantidade de chapas de aluminio: %d\n", chapasAluminio);
-                sleep(2);
-            } else {
-                pthread_cond_broadcast(&condFazerLatinhas);
-                if (chapasAluminio >= CHAPAS) {
-                    pthread_cond_wait(&condChapaAluminio, &lockChapas);
-                }
+                printf("MaqChapaAluminio %d, Quantidade de chapas de aluminio: %d\n", i,
+                       chapasAluminio);
+                sleep(1);
             }
-        pthread_mutex_unlock(&lockChapas);
+        //pthread_mutex_unlock(&lockChapas);
+        sem_post(&mutex);
+        sleep(1);
     }
 }
 
-void* maqFazerLatinha() {
+void* maqFazerLatinha(void* id) {
+    int i = *((int*)id);
     while (TRUE) {
-        pthread_mutex_lock(&lockChapas);
-        pthread_mutex_lock(&lockLatasBasicas);
+        //pthread_mutex_lock(&lockChapas);
+        //printf("maqFazerLatinha %d pegou o lock\n", i);
+        //sleep(1);
+        sem_wait(&mutex);
             if (chapasAluminio >= Y) {
                 chapasAluminio -= Y;
                 latasBasicas += X;
-                printf("Quantidade de latas: %d\n", latasBasicas);
-             //   sleep(2);
-            } else {
-                pthread_cond_broadcast(&condChapaAluminio);
-                pthread_mutex_unlock(&lockLatasBasicas);
-                pthread_cond_wait(&condFazerLatinhas, &lockChapas);
+                printf("MaqFazerLatinha %d, Quantidade de latas: %d\n", i, latasBasicas);
             }
-        pthread_mutex_unlock(&lockLatasBasicas);
-        pthread_mutex_unlock(&lockChapas);
+        sem_post(&mutex);
+        sleep(1);
+        //pthread_mutex_unlock(&lockChapas);
     }
 }
 
 int main() {
 
+    int* id;
+
     pthread_t maq1[MAQ1];
     pthread_t maq2[MAQ2];
 
-    for (int i = 0; i < MAQ1; i++) {
-        pthread_create(&maq1[i], NULL, maqChapaAlumunio, NULL);
-    }
+    sem_init(&mutex, 0, 1);
+
     for (int i = 0; i < MAQ2; i++) {
-        pthread_create(&maq2[i], NULL, maqFazerLatinha(), NULL);
+        id = (int*) malloc(sizeof(int));
+        *id = i;
+        pthread_create(&maq2[i], NULL, maqFazerLatinha, (void*)id);
+    }
+    for (int i = 0; i < MAQ1; i++) {
+        id = (int*) malloc(sizeof(int));
+        *id = i;
+        pthread_create(&maq1[i], NULL, maqChapaAlumunio, (void*)id);
     }
 
     for (int i = 0; i < MAQ1; i++) {
